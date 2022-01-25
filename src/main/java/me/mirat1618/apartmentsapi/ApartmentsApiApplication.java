75a9c423bc3ff2d18375a8c3e2df50cd3389100b
mirat1618 <mirat1618@gmail.com>
2022-01-25 09:05:09 +0500

    initial commit

---
+++ src/main/java/me/mirat1618/apartmentsapi/ApartmentsApiApplication.java
@@ -0,0 +1,13 @@
+package me.mirat1618.apartmentsapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApartmentsApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApartmentsApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/me/mirat1618/apartmentsapi/ApartmentsApiApplicationTests.java
@@ -0,0 +1,13 @@
+package me.mirat1618.apartmentsapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApartmentsApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
