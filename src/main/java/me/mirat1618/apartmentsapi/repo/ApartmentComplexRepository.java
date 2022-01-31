d13349c5a7423b258b78a9c41af84bc98a639bd0
mirat1618 <mirat1618@gmail.com>
2022-01-31 15:15:46 +0500

    add ordering by id to apartment complex repo

--- src/main/java/me/mirat1618/apartmentsapi/repo/ApartmentComplexRepository.java
+++ src/main/java/me/mirat1618/apartmentsapi/repo/ApartmentComplexRepository.java
@@ -3,5 +3,8 @@ package me.mirat1618.apartmentsapi.repo;
 import me.mirat1618.apartmentsapi.model.ApartmentComplex;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface ApartmentComplexRepository extends JpaRepository<ApartmentComplex, Long> {
+    public List<ApartmentComplex> findAllByOrderByIdAsc();
 }
